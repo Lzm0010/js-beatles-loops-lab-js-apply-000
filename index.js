4dbb29b54869d41f1368c232895a70b403ac52eb
Lee McCormick <lzm0010@gmail.com>
2020-01-02 14:23:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function theBeatlesPlay(musicians, instruments){
   var facts = [];
   
   for(i = 0; i < musicians.length; i++) {
-    return `${musicians[i]} plays ${instruments[1]}`
+    facts`${musicians[i]} plays ${instruments[1]}`
   }
 }
